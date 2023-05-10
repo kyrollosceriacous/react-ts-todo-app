d925888423647fa3e7a3aa63a4797897862b3e85
kyrollosceriacous <kyrolosceriacous@gmail.com>
2023-05-10 12:26:19 +0200

    Minor clean up code and identiation

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,34 +5,43 @@ import { Button } from 'react-bootstrap';
 interface InputFieldProps {
     todos: TodoItemModel[];
     setTodos: React.Dispatch<React.SetStateAction<TodoItemModel[]>>;
-}
+};
 
 const InputField: React.FC<InputFieldProps> = ({ todos, setTodos }) => {
     const [inputValue, setInputValue] = useState<string>("");
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue(e.target.value)
-    }
+        setInputValue(e.target.value);
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            createTodo();
+        }
+    };
 
     const createTodo = () => {
         if (inputValue.trim() !== '') {
-        const newTodo: TodoItemModel = {
-            id: Date.now(),
-            text: inputValue,
-            completed: false
-        };
-        setTodos([...todos, newTodo]);
-        setInputValue('');
+            const newTodo: TodoItemModel = {
+                id: Date.now(),
+                text: inputValue,
+                completed: false
+            };
+            setTodos([...todos, newTodo]);
+            setInputValue('');
         }
-    }
+    };
 
     return (
         <div className="input-field">
-        <p>Create a new todo:</p>
-        <input type="text" value={inputValue} onChange={handleInputChange} />
-        <Button className="button-create-todo" onClick={createTodo}>
-            Create todo
-        </Button>
+            <p>Create a new todo:</p>
+            <input type="text" value={inputValue} onChange={handleInputChange} onKeyDown={handleKeyDown} />
+            <Button
+                className='button-create-todo'
+                onClick={createTodo}
+            >
+                Create todo
+            </Button>
         </div>
     );
 };
