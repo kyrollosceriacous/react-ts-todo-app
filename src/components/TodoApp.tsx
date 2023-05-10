931528c07825b598bb540007d1c0d6602843f122
kyrollosceriacous <kyrolosceriacous@gmail.com>
2023-05-10 10:00:28 +0200

    Added filter button to top right corner of page

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -2,28 +2,42 @@ import React, { useState } from "react";
 import InputField from "./InputField";
 import TodoItemModel from "../models/TodoItem";
 import TodoList from "./TodoList";
+import FilterButton from "./FilterButton";
 
 const TodoApp: React.FC = () => {
     const [todos, setTodos] = useState<TodoItemModel[]>([]);
+    const [filter, setFilter] = useState<'all' | 'completed' | 'active'>('all');
 
-    return (
-    <div className="center-div">
-    <div className="big-div">
-        <div className="child-div">
-        <div id="todo-creation">
-            <h1 className="main-heading-app">Todo web app</h1>
-            <InputField todos={todos} setTodos={setTodos}/>
-        </div>  
-        <div id="new-div">
-            <p>hello there general kenobi</p>
+    const filteredTodos = todos.filter(todo => {
+        if (filter === 'all') return true;
+        if (filter === 'completed') return todo.completed
+        if (filter === 'active') return !todo.completed
+        return false;
+    });
+
+  const filterTodos = (filter: 'all' | 'completed' | 'active') => {
+    setFilter(filter);
+  };
+
+  return (
+    <div className='center-div'>
+      <div className='big-div'>
+        <div className='child-div'>
+          <div id='todo-creation'>
+            <h1 className='main-heading-app'>Todo web app</h1>
+            <InputField todos={todos} setTodos={setTodos} />
+            <FilterButton filterTodos={filterTodos} />
+          </div>
+          <div id='new-div'>
+            <p>Hello there general kenobi</p>
+          </div>
         </div>
+        <div className='child-div' id='display-list'>
+          <TodoList todos={filteredTodos} setTodos={setTodos} />
         </div>
-        <div className="child-div" id="display-list">
-        <TodoList todos={todos} setTodos={setTodos}/>
-        </div>
+      </div>
     </div>
-    </div>
-    )
-}
+  );
+};
 
 export default TodoApp;
