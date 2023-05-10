86b029f78802e391f99c4cc5342edb03adad5a29
kyrollosceriacous <kyrolosceriacous@gmail.com>
2023-05-10 10:50:40 +0200

    Can now filter through the todos based on their completion

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,23 +1,19 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import InputField from "./InputField";
 import TodoItemModel from "../models/TodoItem";
 import TodoList from "./TodoList";
-import FilterButton from "./FilterButton";
 
 const LOCAL_STORAGE_KEY = "todos"
 
 const TodoApp: React.FC = () => {
     const [todos, setTodos] = useState<TodoItemModel[]>([]);
-    const [filter, setFilter] = useState<'all' | 'completed' | 'active'>('all');
 
     useEffect(() => {
         const loadedTodos = localStorage.getItem(LOCAL_STORAGE_KEY);
         if (loadedTodos) {
           const parsedTodos = JSON.parse(loadedTodos);
-          console.log('Loaded todos from localStorage:', parsedTodos);
           setTodos(parsedTodos);
-        } else {
-          console.log('No todos found in localStorage');
         }
       }, []);
 
@@ -27,36 +23,38 @@ const TodoApp: React.FC = () => {
         }
     }, [todos]);
 
-    const filteredTodos = todos.filter(todo => {
-        if (filter === 'all') return true;
-        if (filter === 'completed') return todo.completed
-        if (filter === 'active') return !todo.completed
-        return false;
-    });
+    return (
+      <Router>
+        <div className='center-div'>
+          <div className='big-div'>
+            <div className='child-div'>
+              <div id='todo-creation'>
+                <h1 className='main-heading-app'>Todo web app</h1>
+                <InputField todos={todos} setTodos={setTodos} />
 
-  const filterTodos = (filter: 'all' | 'completed' | 'active') => {
-    setFilter(filter);
-  };
-
-  return (
-    <div className='center-div'>
-      <div className='big-div'>
-        <div className='child-div'>
-          <div id='todo-creation'>
-            <h1 className='main-heading-app'>Todo web app</h1>
-            <InputField todos={todos} setTodos={setTodos} />
-            <FilterButton filterTodos={filterTodos} />
-          </div>
-          <div id='new-div'>
-            <p>Btn: Clear currently viewing list?</p>
+                <nav>
+                  <ul>
+                    <li><Link to="/all">All</Link></li>
+                    <li><Link to="/active">Active</Link></li>
+                    <li><Link to="/completed">Completed</Link></li>
+                  </ul>
+                </nav>
+              </div>
+              <div id='new-div'>
+                <p>Btn: Clear currently viewing list?</p>
+              </div>
+            </div>
+            <div className='child-div' id='display-list'>
+              <Routes>
+                <Route path="/all" element={<TodoList todos={todos} setTodos={setTodos} />} />
+                <Route path="/active" element={<TodoList todos={todos.filter(todo => !todo.completed)} setTodos={setTodos} />} />
+                <Route path="/completed" element={<TodoList todos={todos.filter(todo => todo.completed)} setTodos={setTodos} />} />
+              </Routes>
+            </div>
           </div>
         </div>
-        <div className='child-div' id='display-list'>
-          <TodoList todos={filteredTodos} setTodos={setTodos} />
-        </div>
-      </div>
-    </div>
-  );
+      </Router>
+    );
 };
 
 export default TodoApp;
